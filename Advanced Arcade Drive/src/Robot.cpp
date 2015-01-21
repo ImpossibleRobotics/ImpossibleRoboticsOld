190692723113bca6e2763bef43d152ed8aea57ba
Dylan Vos <dylan@vossystems.nl>
2015-01-21 14:57:12 +0100

    Added Advanced Arcade Drive project

---
+++ Advanced Arcade Drive/src/Robot.cpp
@@ -0,0 +1,60 @@
+#include "WPILib.h"
+
+class Robot: public SampleRobot
+{
+	Talon frontLeft, frontRight, rearLeft, rearRight;
+	Joystick stick;
+
+	double ExpirationTime = 0.1;
+	double UpdatePeriod = 0.005;
+	double SpeedModifier = 0;
+	double SpeedModifierLeft = 0;
+	double SpeedModifierRight = 0;
+
+public:
+	Robot() :
+		frontLeft(1),
+		frontRight(3),
+		rearLeft(0),
+		rearRight(2),
+		stick(0)
+	{
+		//Enable safety for all motors
+		frontLeft.SetSafetyEnabled(true);
+		frontRight.SetSafetyEnabled(true);
+		rearLeft.SetSafetyEnabled(true);
+		rearRight.SetSafetyEnabled(true);
+
+		//Set Expiration Time for all motors
+		frontLeft.SetExpiration(ExpirationTime);
+		frontRight.SetExpiration(ExpirationTime);
+		rearLeft.SetExpiration(ExpirationTime);
+		rearRight.SetExpiration(ExpirationTime);
+	}
+
+	void OperatorControl()
+	{
+		while (IsOperatorControl() && IsEnabled())
+		{
+			//Calculate SpeedModifier using given input from throttle
+			SpeedModifier = -0.5*(stick.GetThrottle() - 1);
+
+			//Calculate SpeedModifiers for the left and right motors based on inputs
+			SpeedModifierLeft = stick.GetY() - stick.GetX();
+			SpeedModifierRight = stick.GetY() + stick.GetX();
+
+			//Invert Left Motors
+			SpeedModifierLeft *= -1;
+
+			//Set speed of motors
+			frontLeft.Set(SpeedModifier*SpeedModifierLeft);
+			rearLeft.Set(SpeedModifier*SpeedModifierLeft);
+			frontRight.Set(SpeedModifier*SpeedModifierRight);
+			rearRight.Set(SpeedModifier*SpeedModifierRight);
+
+			Wait(UpdatePeriod);
+		}
+	}
+};
+
+START_ROBOT_CLASS(Robot);
