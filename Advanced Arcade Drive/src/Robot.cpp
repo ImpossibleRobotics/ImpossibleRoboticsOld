17c2a3713bcb8bfd45cfbf717305200c3bbebe7c
Dylan Vos <dylan@vossystems.nl>
2015-02-06 09:53:33 +0100

    Made some small changes

--- Advanced Arcade Drive/src/Robot.cpp
+++ Advanced Arcade Drive/src/Robot.cpp
@@ -16,6 +16,9 @@ class Robot: public SampleRobot
 	double SpeedModifierRight = 0;
 
 public:
+
+
+
 	Robot() :
 		frontLeft(1),
 		frontRight(3),
@@ -23,7 +26,7 @@ public:
 		rearRight(2),
 		arcadeStick(0),
 		gamePad(1),
-		toteLift(5, 0, 1, 2)
+		toteLift(4, 0, 1, 2)
 	{
 		//Enable safety for all motors
 		frontLeft.SetSafetyEnabled(true);
@@ -41,45 +44,45 @@ public:
 	void OperatorControl()
 	{
 		while (IsOperatorControl() && IsEnabled())
-		{
-			if(gamePad.GetRawButton(1) == 1)
-			{
-				toteLift.StackUp();
-			}
-			if(gamePad.GetRawButton(2) == 1)
-			{
-				toteLift.StackDown();
-			}
-			if(gamePad.GetRawButton(3) == 1)
-			{
-				toteLift.StackTotes();
-			}
+				{
+					if(gamePad.GetRawButton(1) == 1)
+					{
+						toteLift.StackUp();
+					}
+					if(gamePad.GetRawButton(2) == 1)
+					{
+						toteLift.StackDown();
+					}
+					if(gamePad.GetRawButton(3) == 1)
+					{
+						toteLift.StackTotes();
+					}
 
 
-			//Calculate SpeedModifier using given input from throttle
-			SpeedModifier = (arcadeStick.GetThrottle() - 1)/-2;
+					//Calculate SpeedModifier using given input from throttle
+					SpeedModifier = (arcadeStick.GetThrottle() - 1)/-2;
 
-			//Calculate SpeedModifiers for the left and right motors based on inputs
-			SpeedModifierLeft = arcadeStick.GetY() - arcadeStick.GetX();
-			SpeedModifierRight = arcadeStick.GetY() + arcadeStick.GetX();
+					//Calculate SpeedModifiers for the left and right motors based on inputs
+					SpeedModifierLeft = arcadeStick.GetY() - arcadeStick.GetX();
+					SpeedModifierRight = arcadeStick.GetY() + arcadeStick.GetX();
 
-			//Invert Left Motors
-			SpeedModifierLeft *= -1;
+					//Invert Left Motors
+					SpeedModifierLeft *= -1;
 
-			//Set speed of motors
-			frontLeft.Set(SpeedModifier*SpeedModifierLeft);
-			rearLeft.Set(SpeedModifier*SpeedModifierLeft);
-			frontRight.Set(SpeedModifier*SpeedModifierRight);
-			rearRight.Set(SpeedModifier*SpeedModifierRight);
+					//Set speed of motors
+					frontLeft.Set(SpeedModifier*SpeedModifierLeft);
+					rearLeft.Set(SpeedModifier*SpeedModifierLeft);
+					frontRight.Set(SpeedModifier*SpeedModifierRight);
+					rearRight.Set(SpeedModifier*SpeedModifierRight);
 
-			SmartDashboard:: PutNumber("SpeedModifier", SpeedModifier);
-			SmartDashboard:: PutNumber("SpeedModifierLeft", SpeedModifierLeft);
-			SmartDashboard:: PutNumber("SpeedModifierRight", SpeedModifierRight);
-			SmartDashboard:: PutNumber("Total speed Left", SpeedModifier * SpeedModifierLeft);
-			SmartDashboard:: PutNumber("Total speed Right", SpeedModifier * SpeedModifierRight);
+					SmartDashboard:: PutNumber("SpeedModifier", SpeedModifier);
+					SmartDashboard:: PutNumber("SpeedModifierLeft", SpeedModifierLeft);
+					SmartDashboard:: PutNumber("SpeedModifierRight", SpeedModifierRight);
+					SmartDashboard:: PutNumber("Total speed Left", SpeedModifier * SpeedModifierLeft);
+					SmartDashboard:: PutNumber("Total speed Right", SpeedModifier * SpeedModifierRight);
 
-			Wait(UpdatePeriod);
-		}
+					Wait(UpdatePeriod);
+				}
 	}
 
 };
