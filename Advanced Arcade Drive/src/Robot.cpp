4b8de0d0eab4e2f8b92675ede36e4ced36483e10
UrsaPolaris <kerwinphillips26@gmail.com>
2015-02-13 13:09:36 +0100

    auto nog een keer, werk nog niet

--- Advanced Arcade Drive/src/Robot.cpp
+++ Advanced Arcade Drive/src/Robot.cpp
@@ -37,6 +37,48 @@ public:
 		rearLeft.SetExpiration(ExpirationTime);
 		rearRight.SetExpiration(ExpirationTime);
 	}
+	// drive functions
+	 void forward(int time, float power) {
+	  Timer *autonTimer;
+	  autonTimer = new Timer();
+	  autonTimer->Start();
+
+	  while(autonTimer->Get() > time){
+	   frontLeft.Set(-1.2*power);
+	   rearLeft.Set(-1.2*power);
+	   frontRight.Set(power);
+	   rearRight.Set(power);
+	  }
+	  autonTimer->Reset();
+	 }
+
+
+	 void right(int time, int power){
+	  left(time, -power);
+	 }
+
+	 void left(int time, float power) {
+	  Timer *autonTimer;
+	  autonTimer = new Timer();
+	  autonTimer->Start();
+
+	  while(autonTimer->Get() > time){
+	   frontLeft.Set(power);
+	   rearLeft.Set(power);
+	   frontRight.Set(power);
+	   rearRight.Set(power);
+	  }
+	  autonTimer->Reset();
+	 }
+
+	 void Autonomous(){
+	  forward(1, 0.4);
+	  toteLift.StackUp();
+	  forward(1,0);
+	  toteLift.StackTotes();
+	  forward(1,-0.2);
+	 }
+
 
 	void OperatorControl()
 	{
@@ -83,50 +125,6 @@ public:
 			Wait(UpdatePeriod);
 		}
 	}
-	// drive functions
-	 void forward(int time, float power) {
-	  time = time * 1000000;
-	  Timer *autonTimer;
-	  autonTimer = new Timer();
-	  autonTimer->Start();
-
-	  while(autonTimer->Get() > time){
-	   frontLeft.Set(-1.2*power);
-	   rearLeft.Set(-1.2*power);
-	   frontRight.Set(power);
-	   rearRight.Set(power);
-	  }
-	  autonTimer->Reset();
-	 }
-
-
-	 void right(int time, int power){
-	  left(time, -power);
-	 }
-
-	 void left(int time, float power) {
-	  time = time * 1000000;
-	  Timer *autonTimer;
-	  autonTimer = new Timer();
-	  autonTimer->Start();
-
-	  while(autonTimer->Get() > time){
-	   frontLeft.Set(power);
-	   rearLeft.Set(power);
-	   frontRight.Set(power);
-	   rearRight.Set(power);
-	  }
-	  autonTimer->Reset();
-	 }
-
-	 void Autonomous(){
-	  forward(1, 0.4);
-	  toteLift.StackUp();
-	  forward(1, 0.2);
-	  toteLift.StackTotes();
-	  forward(0.6, -0.3);
-	 }
-
 
 };
 
